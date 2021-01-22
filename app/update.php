7480289bef4c84f0a3e12849e106ec682b0346f7
Jonathan <rivo.jonathan98@gmail.com>
2021-01-22 12:21:15 +0100

    Added update function

--- app/update.php
+++ app/update.php
@@ -1,2 +1,26 @@
-# todo-list
-Code to practice git
+<?php
+
+if (isset($_POST['targetName']) && isset($_POST['newName'])) {
+    require '../db_connect.php';
+
+    $target = $_POST['targetName'];
+    $newName = $_POST['newName'];
+
+    if (empty($target) || empty($newName)) {
+        header("Location: ../index.php?mess=error");
+    } else {
+        // $stmt = $conn->prepare("INSERT INTO todos(title) VALUE(?)");
+        $stmt = $conn->prepare("UPDATE todos SET title = (?) WHERE title = (?)");
+        $res = $stmt->execute([$newName,$target]);
+
+        if ($res) {
+            header("Location: ../index.php?mess=success");
+        } else {
+            header("Location: ../index.php");
+        }
+        $conn = null;
+        exit();
+    }
+} else {
+    header("Location: ../index.php?mess=error");
+}
